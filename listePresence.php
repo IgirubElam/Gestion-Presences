9e4977ed8dceb264637158fd43e2fb1dba9c6867
Elam Igirubuntu <108549888+IgirubElam@users.noreply.github.com>
2023-03-24 11:57:51 +0200

    Add files via upload

--- listePresence.php
+++ listePresence.php
@@ -8,7 +8,25 @@
 		header('location:login.php');
 	}
 
-	
+	// if (setlocale(LC_TIME, 'fr_FR') == '') {
+	// 	setlocale(LC_TIME, 'FRA');
+	// 	$format_jour = '%#d';
+	// }else{
+	// 	$format_jour = '%e';
+	// }
+
+	// $requete="select * from  presence,appel,type_activite,choriste where presence.Id_appel=appel.Id_appel AND appel.Id_type_activite =type_activite.Id_activite AND presence.Id_choriste=choriste.Id_choriste";
+	// $resultat=$BaseDonnee->query($requete); 
+
+
+	// $typeActivite = $_GET['Nom_type_activite'];
+	// $sql = "SELECT c.Nom, c.Prenom, a.Description_appel
+	// 		FROM choriste c
+	// 		JOIN presence p ON c.Id_choriste = p.Id_choriste
+	// 		JOIN appel a ON p.Id_appel = a.Id_appel 
+	// 		WHERE a.Id_type_activite = '$typeActivite' ";
+	// $resultat = $BaseDonnee->query($sql);
+
 ?>
 
 <!DOCTYPE html>
@@ -32,7 +50,7 @@
 			<div class="panel-heading"> Liste des presences</div>
 			<div class="panel-body">
 
-				<button type="button" class="btn btn-primary" onclick="getPresence('Repetition')">Repetition</button>
+				<button type="button" class="btn btn-primary" id="resultats" onclick="getPresence('Repetition')">Repetition</button>
 				&nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp
 				&nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp;
 				<button class="btn btn-primary">Culte Francophone</button>
@@ -43,21 +61,7 @@
 				&nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp;
 				<button class="btn btn-primary">Louange</button>
 				
-					<table class="table table-hover" id="table-presence" style="display: none;">
-						<thead>
-							<tr>
-								<th>Appel </th>
-								<th>Nom </th> 
-								<th>Prenom</th>
-							</tr>
-						</thead>
-
-						<tbody>
-							<tr>
-								
-							</tr>
-						</tbody>
-					</table> 
+					
 				
 			</div>
 		</div>
@@ -67,23 +71,49 @@
 	<script type="text/javascript">
 
 		
-		function getPresence(type_activite){
+		function getPresence(typeActivite){
 			$.ajax({
-					url: 'get_presence.php',
-					type:'POST';
-					data:{type_activite: type_activite},
+					url: "get_presence.php",
+					type:"POST",
+					data:{type_activite: typeActivite},
+					dataType: "json",
 					success:function(data){
-						$("#table-presence").html(data);
 
-						$("#table-presence").show();
+						if (data.length > 0) {
+							var html = "<table>";
+						html += "<tr> 
+							      <th> Nom </th>
+							      <th> Prenom</th>
+							      <th> Description appel</th>
+							      </tr>;
+							      for (var i = 0; i < data.length; i++) {
+							      	html += "<tr>";
+							      	html += "<td>" + data[i].Nom + "</td>";
+									html += "<td>" + data[i].Prenom + "</td>";
+							      	html += "<td>" + data[i].Description_appel + "</td>";
+
+							      	html += "</tr>";
+							      }
+							      html += "</table>";
+
+							      $("#resultats").html(html);
+						} else {
+							$("#resultats").html("0 resultats");
+						}
 					},
-					error:function(jqXHR, testStatus, errorThrown) {
-						console.log(textStatus, errorThrown);
+						error:function() {
+						alert("Erreur lors de la recuperation des donnees."); 
 					}
-				});
+					});
+					
 		}
 
-		;
+
+		// $(document).ready(function(){
+		// 	$("#btn-repetition").click(function(){
+				
+		// 	});
+		// });
 	</script>
 
 	<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popped.min.js"></script>
